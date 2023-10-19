a6a34bbc2b501ce58dc669a053c3ad0e9ead1332
noahJamal89 <noah_jamal_89nj@outlook.com>
2023-10-19 09:15:27 -0700

    noah

--- bellAdoptionCenter/src/main/java/com/CenterOfAdoption/bellAdoptionCenter/DAO/LookupPetDao.java
+++
@@ -1,9 +0,0 @@
-package com.CenterOfAdoption.bellAdoptionCenter.DAO;
-
-import com.CenterOfAdoption.bellAdoptionCenter.MODEL.LookupPet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LookupPetDao extends JpaRepository<LookupPet,Long> {
-}
--- bellAdoptionCenter/src/main/java/com/CenterOfAdoption/bellAdoptionCenter/DAO/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.CenterOfAdoption.bellAdoptionCenter.DAO;
-
-import com.CenterOfAdoption.bellAdoptionCenter.MODEL.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentDao extends JpaRepository<Student,Long> {
-}
